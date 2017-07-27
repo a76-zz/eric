7066a1185b1d1280dcfc9747769465f9a68d55f7
Andrei Silchankau <andrei.silchankau@thomsonreuters.com>
2017-07-27 19:40:29 +0300

    cosmetic design update

--- components/Layout/Categories.js
+++ components/Layout/Categories.js
@@ -1,16 +1,32 @@
 import React from 'react'
 import Link from '../Link'
 import s from './Categories.css'
+import cx from 'classnames'
 
 const Categories = () => (
   <aside className={s.sidebar}>
     <h2 className={s.title}>You</h2>
     <ul className={s.list}>
+      <li className={cx(s.item, s.active)}>
+        <Link to="">In progress</Link>
+      </li>
       <li className={s.item}>
-        <Link to="">Continue</Link>
+        <Link to="">Your Shows</Link>
+      </li>
+      <li className={s.item}>
+        <Link to="">Your Channels</Link>
+      </li>
+      <li className={s.item}>
+        <Link to="">You Might Like</Link>
+      </li>
+      <li className={s.item}>
+        <Link to="">Recently Aired</Link>
       </li>
       <li className={s.item}>
         <Link to="">Favorites</Link>
+      </li>
+      <li className={s.item}>
+        <Link to="">Bookmarks</Link>
       </li>
       <li className={s.item}>
         <Link to="">Purchases</Link>
